ce3e6a3820749baf752933734bd044766cff9e7a
alirangkul <ali.nurcahya@barangkulakan.com>
2024-02-29 18:57:39 +0700

    initial project

---
+++ main.js
@@ -0,0 +1,34 @@
+// 1. Menggunakan 5 variable untuk menghitung 3 angka dan outputkan hasilnya
+let angka1 = 10 + 10 + 10;
+console.log(angka1);
+
+let angka2 = 10 * 10 / 10;
+console.log(angka2);
+
+let angka3 = 5 + 5 + 5;
+console.log(angka3);
+
+let angka4 = 5 * 5 / 5;
+console.log(angka4);
+
+let angka5 = 20 + 20 + 20;
+console.log(angka5);
+
+// 2. Membuat masing-masing 2 dari setiap operator perbandingan dan outputkan hasilnya
+let perbandingan1 = 10 > 5;
+console.log(perbandingan1);
+
+let perbandingan2 = 10 < 20;
+console.log(perbandingan2);
+
+let perbandingan3 = 5 >= 5;
+console.log(perbandingan3);
+
+let perbandingan4 = 5 <= 4;
+console.log(perbandingan4);
+
+let perbandingan5 = 20 === 20;
+console.log(perbandingan5);
+
+let perbandingan6 = 20 !== 20;
+console.log(perbandingan6);
